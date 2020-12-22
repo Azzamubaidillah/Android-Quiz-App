d025c126ab31e66c36ae8b5c4d502de5a6cd7d39
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2020-12-22 23:39:00 +0700

    unresolved references error

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnCLickListener {
+
+            if (et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Masukkan nama", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
