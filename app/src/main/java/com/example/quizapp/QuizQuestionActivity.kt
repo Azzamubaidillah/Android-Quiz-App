1658bc4a12da8115e86d1422ba4ab60d81411318
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2020-12-23 23:08:54 +0700

    before forest

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -3,6 +3,9 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,19 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         val questionList = Constants.getQuestions()
         Log.i("Question SIze","${questionList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionList[currentPosition -1]
+
+        findViewById<ProgressBar>(R.id.progressBar).progress = currentPosition
+        findViewById<TextView>(R.id.tv_progress).text = "$currentPosition" + "/" + findViewById<ProgressBar>(R.id.progressBar).max
+
+        findViewById<TextView>(R.id.tv_question).text = question!!.question
+        findViewById<ImageView>(R.id.iv_image).setImageResource(question.image)
+        findViewById<TextView>(R.id.tv_option_one).text = question.optionOne
+        findViewById<TextView>(R.id.tv_option_two).text = question.optionTwo
+        findViewById<TextView>(R.id.tv_option_three).text = question.optionThree
+        findViewById<TextView>(R.id.tv_option_four).text = question.optionFour
+
     }
 }
